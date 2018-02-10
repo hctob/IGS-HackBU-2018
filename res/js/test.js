c2db48db6cfe1429b00862605b751f1bf86a4db3
Daniel Bottcher <Daniel@Daniels-MacBook-Pro.local>
2018-02-10 14:33:24 -0500

    Added updated test.js

--- res/js/test.js
+++ res/js/test.js
@@ -8,15 +8,30 @@ function setup() {
 
 function draw() {
   background(50);
-  spr.velocity.x = (mouseX - spr.position.x) * 0.2;
-  spr.velocity.y = (mouseY - spr.position.y) * 0.2;
+
+  if(mouseIsPressed) {
+  	spr.position.x = (mouseX);
+  	spr.position.y = (mouseY);
+  }
   drawSprites();
 }
 
 function Character() {
+	this.x;
+	this.y;
 
 	this.show = function() {
 		spr = createSprite(width/2, height/2, 40, 40);
   		spr.shapeColor = color(255, 0, 0);
+  		//text("GeebLord", spr.position.x, spr.positon.y + 10);
+  		this.x = spr.position.x;
+  		this.y = spr.position.y;
 	}
+
+	this.interact = function() {
+	}
+}
+
+function Wall() {
+
 }
