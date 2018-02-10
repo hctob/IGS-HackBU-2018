3b65f40d69f1cf0f8ee440ea7443ae9f2ca0e9a2
Daniel Bottcher <Daniel@Daniels-MacBook-Pro.local>
2018-02-10 13:48:33 -0500

    Added main folder structure + p5 + p5.play

--- res/js/test.js
+++ res/js/test.js
@@ -1,8 +1,33 @@
+var player;
+
 function setup() {
 	createCanvas(400, 600);
+	player = new Character();
+	player.show();
 }
 
 function draw() {
 	background(220);
-	
+	drawSprites();
+
+}
+
+function Character() {
+	this.x = 10;
+	this.y = 10;
+	this.xspeed = 0;
+	this.yspeed = 0;
+
+	this.changeDir = function(x,y) {
+		this.xspeed = x;
+		this.yspeed = y;
+	}
+
+	this.show = function() {
+		fill(100);
+	}
+}
+
+function keyPressed() {
+
 }
