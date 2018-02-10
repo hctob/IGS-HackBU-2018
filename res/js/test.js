1c355298f6b3ea186e0a9e02f1f128b121ab154a
Daniel Bottcher <Daniel@Daniels-MacBook-Pro.local>
2018-02-10 16:19:26 -0500

    Message

--- res/js/test.js
+++ res/js/test.js
@@ -3,7 +3,7 @@ var wallSpr;
 var river;
 
 function setup() {
-	createCanvas(1000, 800);
+	createCanvas(windowWidth, 800);
 	player = new Character();
 	player.show();
 	//river = new obj();
@@ -13,29 +13,29 @@ function setup() {
 function draw() {
   background(132, 200, 130);
 
-  if(keyIsDown(UP_ARROW)) {
+  if(keyIsDown(UP_ARROW) && spr.position.y > 20) {
   	spr.position.y -= 5;
   }
-  if(keyIsDown(DOWN_ARROW)) {
+  if(keyIsDown(DOWN_ARROW) && spr.position.y < 800) {
   	spr.position.y += 5;
   }
-  if(keyIsDown(LEFT_ARROW)) {
+  if(keyIsDown(LEFT_ARROW) && spr.position.x > 30) {
   	spr.position.x -= 5;
   }
-  if(keyIsDown(RIGHT_ARROW)) {
+  if(keyIsDown(RIGHT_ARROW) && spr.position.x < windowWidth) {
   	spr.position.x += 5;
   }
 
+  //spr.position.x = constrain(spr.position.x, x, windowWidth);
+  //spr.position.y = constrain(spr.position.y, y, windowHeight);
 
   drawSprites();
 }
 
 function Character() {
-	this.x;
-	this.y;
-
 	this.show = function() {
 		spr = createSprite(width/2, height/2, 40, 40);
+		spr.position.x = constrain(spr.position.x, 0, 800);
   		spr.shapeColor = color(255, 0, 0);
   		//text("GeebLord", spr.position.x, spr.positon.y + 10);
   		//var s = "X: " + this.x + " Y: " + this.y;
